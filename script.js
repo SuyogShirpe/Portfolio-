a1cff0af21af7c8bb62cd5017bff63a4f15c76d8
5uyog <shirpesuyog@gmail.com>
2024-12-18 13:40:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Wait until the page is fully loaded
-window.onload = function() {
-    // Hide the loader
-    document.getElementById('loader').style.display = 'none';
-    // Show the content
-    document.getElementById('content').style.display = 'block';
-  };
-  
+// script.js
+
+window.addEventListener('load', function() {
+  // Hide loader after 2 seconds or when the page finishes loading
+  setTimeout(function() {
+      document.getElementById('loader').style.display = 'none';
+      document.querySelector('.content').style.visibility = 'visible';
+      document.querySelector('.content').style.opacity = 1;
+  }, 2000); // Ensure loader lasts for at least 2 seconds
+});
