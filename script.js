ddb14d70e8b692be9c3b1af4c09ce40acbe2922f
5uyog <shirpesuyog@gmail.com>
2024-12-18 13:01:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Wait until the page is fully loaded
+window.onload = function() {
+    // Hide the loader
+    document.getElementById('loader').style.display = 'none';
+    // Show the content
+    document.getElementById('content').style.display = 'block';
+  };
+  
